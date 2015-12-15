7eaa81e30268e56299519362a50c4f2e5fc4f350
Will Smith <WSmith@scottlogic.com>
2015-12-15 13:20:50 +0000

    Created a function that will convert value into little endian format

--- bitcoin.js
+++ bitcoin.js
@@ -22,8 +22,12 @@ var SwapOrder= function(input) {
 
 var littleEndian=function(value) {
   var valueAsHex=value.toString(16);
-  var hexAsBin=Hex2Bin(valueAsHex);
-  var byteBuffer=new Buffer(valueAsHex,"hex");
+  var paddedValue=valueAsHex;
+  for(var i=0;i<8-valueAsHex.length; i++){
+     paddedValue="0"+paddedValue;
+  }
+
+  var byteBuffer=new Buffer(paddedValue,"hex");
   var tmp;
   var counter=byteBuffer.length - 1;
   for(var i=0; i<(byteBuffer.length/2);i++){
@@ -40,6 +44,3 @@ var littleEndian=function(value) {
 exports.checkHash = checkHash;
 exports.SwapOrder=SwapOrder;
 exports.littleEndian=littleEndian;
-
-function Hex2Bin(n){if(!checkHex(n))return 0;return parseInt(n,16).toString(2)}
-function checkHex(n){return/^[0-9A-Fa-f]{1,64}$/.test(n)}
