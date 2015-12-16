6225bfbbcda06f9568a2320fdae7be0c1342c14b
Will Smith <WSmith@scottlogic.com>
2015-12-16 13:21:09 +0000

    Semicolons everywhere

--- bitcoin.js
+++ bitcoin.js
@@ -1,5 +1,5 @@
-var buffer=require('buffer')
-var crypto=require('crypto')
+var buffer=require('buffer');
+var crypto=require('crypto');
 
 
 var checkHash=function(block) {
@@ -7,7 +7,7 @@ var checkHash=function(block) {
  var merkleroot=reverseEndian(calculateMerkleRoot(block.tx));
 
  var previousBlockHash=((typeof block.previousblockhash==="undefined") ?
- "0000000000000000000000000000000000000000000000000000000000000000" : SwapOrder(block.previousblockhash) )
+ "0000000000000000000000000000000000000000000000000000000000000000" : SwapOrder(block.previousblockhash) );
 
  var time= reverseEndian(block.time);
  var bits=reverseEndian(block.bits);
@@ -20,13 +20,13 @@ var checkHash=function(block) {
  var finalHash=reverseEndian(rehashed);
 
  return block.hash==finalHash;
-}
+};
 
 function hash(value){
   var hash=crypto.createHash('sha256');
   var hashedOnce=hash.update(Hex2Bin(value)).digest('hex');
   return hashedOnce;
-}
+};
 
 var reverseEndian= function(input) {
   var valueAsHex=input.toString(16);
@@ -37,33 +37,33 @@ var reverseEndian= function(input) {
     temp=reversedInput[i+1];
     reversedInput[i+1]=reversedInput[i];
     reversedInput[i] = temp;
-  }
+  };
 
   return reversedInput.join('');
-}
+};
 
 var padBits=function(value) {
   var valueAsHex=value.toString(16);
   var paddedValue=valueAsHex;
   for(var i=0;i<8-valueAsHex.length; i++){
      paddedValue="0"+paddedValue;
-  }
+  };
 
   return paddedValue;
-}
+};
 
 
 var calculateMerkleRoot=function(block){
-  var transactions=block
+  var transactions=block;
 
   if(transactions.length==1){
     return transactions[0]
-  }
+  };
   //If odd number of elements add another node
   if(transactions.length % 2 !=0){
     var leafNode=transactions[transactions.length-1]
     transactions.push(leafNode)
-  }
+  };
 
   var hashedRowAbove=[];
 
@@ -72,20 +72,20 @@ var calculateMerkleRoot=function(block){
     var neighbouringTx= transactions[i+1];
     var parentNode= calculateHashNodeInTree(currentTx,neighbouringTx)
     hashedRowAbove.push(parentNode)
-  }
+  };
 
-  return calculateMerkleRoot(hashedRowAbove)
+  return calculateMerkleRoot(hashedRowAbove);
 
-}
+};
 
 var calculateHashNodeInTree=function(v1,v2){
-  var tx1=reverseEndian(v1)
-  var tx2=reverseEndian(v2)
+  var tx1=reverseEndian(v1);
+  var tx2=reverseEndian(v2);
 
-  var merkleroot=reverseEndian(hash(hash(tx1+tx2)))
+  var merkleroot=reverseEndian(hash(hash(tx1+tx2)));
 
   return merkleroot;
-}
+};
 
 function Hex2Bin(n){
   var hex = n, // ASCII HEX: 37="7", 57="W", 71="q"
@@ -94,12 +94,12 @@ function Hex2Bin(n){
 
   for(var i=0; i< hex.length-1; i+=2){
       bytes.push(parseInt(hex.substr(i, 2), 16));
-  }
+  };
 
   str = String.fromCharCode.apply(String, bytes);
 
   return str;
-}
+};
 
 exports.calculateMerkleRoot = calculateMerkleRoot;
 exports.calculateHashNodeInTree=calculateHashNodeInTree;
